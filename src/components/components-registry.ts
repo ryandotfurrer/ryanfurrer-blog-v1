60f1968fa4ecd8f8b2424909baa71920a045a83a
Stackbit Code Editor <projects@stackbit.com>
2022-01-28 01:53:26 +0000

    components-registry.ts: changed

--- src/components/components-registry.ts
+++ src/components/components-registry.ts
@@ -44,6 +44,7 @@ const components = {
     'ImageBlock': dynamic(() => import('./molecules/ImageBlock')),
     'JobsSection': dynamic(() => import('./sections/JobsSection')),
     'MediaGallerySection': dynamic(() => import('./sections/MediaGallerySection')),
+    
     'PostFeedSection': dynamic(() => import('./sections/PostFeedSection')),
     'FeaturedPostsSection': dynamic(() => import('./sections/FeaturedPostsSection')),
     'RecentPostsSection': dynamic(() => import('./sections/RecentPostsSection')),

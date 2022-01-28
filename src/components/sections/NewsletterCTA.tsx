c3f381e75d450c6971867fad65ce1f2d86653969
Stackbit Code Editor <projects@stackbit.com>
2022-01-28 02:27:46 +0000

    NewsletterCTA.tsx: changed

--- src/components/sections/NewsletterCTA.tsx
+++ src/components/sections/NewsletterCTA.tsx
@@ -11,7 +11,8 @@ const NewsletterCTA = ({ text }) => {
                         <div className="w-full lg:flex-grow">
                             <h2 className="text-left" data-sb-field-path=".title">Like what you've read?</h2>
                             <div className="sb-markdown sm:text-lg text-left mt-4" data-sb-field-path=".text">
-                                <p>Subscribe to stay in the know for when I publish new articles, find interesting things on the web, and more!</p></div></div><div className="w-full"><div className="overflow-x-hidden"><div className="flex flex-wrap items-center -mx-2 lg:flex-nowrap justify-start" data-sb-field-path=".actions"><a href="https://www.getrevue.co/profile/theryanfurrer/" aria-label="Subscribe to The Ryan Furrer Newsletter" className="sb-component sb-component-block sb-component-button mb-3 mx-2 lg:whitespace-nowrap sb-component-button-primary" data-sb-field-path=".0 .0.url#@href .0.altText#@aria-label .0.elementId#@id .0.label#span[1] .0.icon#svg[1]"><span>Subscribe</span></a></div></div></div></div></div></div></div></div>
+                                <p>Subscribe to stay in the know for when I publish new articles, find interesting things on the web, and more!</p>
+                                </div></div><div className="w-full"><div className="overflow-x-hidden"><div className="flex flex-wrap items-center -mx-2 lg:flex-nowrap justify-start" data-sb-field-path=".actions"><a href="https://www.getrevue.co/profile/theryanfurrer/" aria-label="Subscribe to The Ryan Furrer Newsletter" className="sb-component sb-component-block sb-component-button mb-3 mx-2 lg:whitespace-nowrap sb-component-button-primary" data-sb-field-path=".0 .0.url#@href .0.altText#@aria-label .0.elementId#@id .0.label#span[1] .0.icon#svg[1]"><span>Subscribe</span></a></div></div></div></div></div></div></div></div>
     )
 }
 

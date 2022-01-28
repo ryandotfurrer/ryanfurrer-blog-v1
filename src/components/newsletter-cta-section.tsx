34b4467ccd177ba605bf536deb15e550a35cdc0b
Stackbit Code Editor <projects@stackbit.com>
2022-01-28 01:52:22 +0000

    newsletter-cta-section.tsx: changed

--- src/components/newsletter-cta-section.tsx
+++ src/components/newsletter-cta-section.tsx
@@ -2,13 +2,13 @@
 import React from 'react'
 
 const newsletterCTA = ({ text }) => {
-  return (
-    <div className="py-6 bg-primary">
-      <div className="container mx-auto">
-        <p className="text-center text-xl text-white">{text}</p>
-      </div>
-    </div>
-  )
+    return (
+        <div className="py-6 bg-primary">
+            <div className="container mx-auto">
+                <p className="text-center text-xl text-white">{text}</p>
+            </div>
+        </div>
+    )
 }
 
 export default newsletterCTA

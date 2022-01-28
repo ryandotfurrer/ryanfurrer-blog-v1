4a489763fa21ce05d663448e063ef611179ba22b
Stackbit Code Editor <projects@stackbit.com>
2022-01-28 03:03:45 +0000

    NewsletterCTA.tsx: changed

--- src/components/sections/NewsletterCTA.tsx
+++ src/components/sections/NewsletterCTA.tsx
@@ -8,7 +8,7 @@ import Action from '../atoms/Action';
 
 export default function NewsletterCTA(props) {
     const cssId = props.elementId || null;
-    const colors = props.colors || 'colors-c';
+    const colors = props.colors || 'colors-d';
     const bgSize = props.backgroundSize || 'inset';
     const sectionStyles = props.styles?.self || {};
     const sectionWidth = sectionStyles.width || 'wide';

9d5bf6c0899e8cc219bac30929767dcaed6b6d8e
Stackbit Code Editor <projects@stackbit.com>
2022-01-28 02:09:16 +0000

    index.tsx: changed

--- src/components/layouts/DefaultBaseLayout/index.tsx
+++ src/components/layouts/DefaultBaseLayout/index.tsx
@@ -20,6 +20,7 @@ export default function DefaultBaseLayout(props) {
                 </Head>
                 {site.header && <Header {...site.header} annotationPrefix={siteMeta.id} />}
                 {props.children}
+                {<}
                 {site.footer && <Footer {...site.footer} annotationPrefix={siteMeta.id} />}
             </div>
         </div>

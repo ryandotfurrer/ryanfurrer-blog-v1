69b2ff7002941717c16e2ad8af7857b1e24f1349
Stackbit Code Editor <projects@stackbit.com>
2022-01-29 02:53:02 +0000

    NewsletterCTA.tsx: changed

--- src/components/sections/NewsletterCTA.tsx
+++ src/components/sections/NewsletterCTA.tsx
@@ -182,6 +182,7 @@ function ctaBody(props) {
                             </div>
                         </form>
                         </div>
+                        </div>
 
                     
                 )

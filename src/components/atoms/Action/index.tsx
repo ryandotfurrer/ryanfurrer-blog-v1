8a61c724444265d15121529cee239e312ad3be51
Stackbit Code Editor <projects@stackbit.com>
2022-01-29 03:18:26 +0000

    index.tsx: changed

--- src/components/atoms/Action/index.tsx
+++ src/components/atoms/Action/index.tsx
@@ -66,27 +66,27 @@ export default function Action(props) {
     const cssClasses = props.className || null;
     const cssId = props.elementId || null;
 
-    // return (
-    //     <Link
-    //         href={url}
-    //         aria-label={altText}
-    //         id={cssId}
-    //         className={classNames('sb-component', 'sb-component-block', style === 'link' ? 'sb-component-link' : 'sb-component-button', cssClasses, {
-    //             'sb-component-button-primary': style === 'primary',
-    //             'sb-component-button-secondary': style === 'secondary'
-    //         })}
-    //         data-sb-field-path={annotations.join(' ').trim()}
-    //     >
-    //         {label && <span>{label}</span>}
-    //         {showIcon && IconComponent && (
-    //             <IconComponent
-    //                 className={classNames('fill-current h-5 w-5', {
-    //                     'order-first': iconPosition === 'left',
-    //                     'mr-1.5': label && iconPosition === 'left',
-    //                     'ml-1.5': label && iconPosition === 'right'
-    //                 })}
-    //             />
-    //         )}
-    //     </Link>
-    // );
+    return (
+        <Link
+            href={url}
+            aria-label={altText}
+            id={cssId}
+            className={classNames('sb-component', 'sb-component-block', style === 'link' ? 'sb-component-link' : 'sb-component-button', cssClasses, {
+                'sb-component-button-primary': style === 'primary',
+                'sb-component-button-secondary': style === 'secondary'
+            })}
+            data-sb-field-path={annotations.join(' ').trim()}
+        >
+            {label && <span>{label}</span>}
+            {showIcon && IconComponent && (
+                <IconComponent
+                    className={classNames('fill-current h-5 w-5', {
+                        'order-first': iconPosition === 'left',
+                        'mr-1.5': label && iconPosition === 'left',
+                        'ml-1.5': label && iconPosition === 'right'
+                    })}
+                />
+            )}
+        </Link>
+    );
 }

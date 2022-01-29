bab0e8b4827d617c768986016009a5204225023f
Stackbit Code Editor <projects@stackbit.com>
2022-01-29 02:55:45 +0000

    NewsletterCTA.tsx: changed

--- src/components/sections/NewsletterCTA.tsx
+++ src/components/sections/NewsletterCTA.tsx
@@ -120,111 +120,111 @@ function ctaBody(props) {
                             {props.title}
                         </h2>
                         <div id="revue-embed">
-  <form
-    action="https://www.getrevue.co/profile/theryanfurrer/add_subscriber"
-    method="post"
-    id="revue-form"
-    name="revue-form"
-    target="_blank"
-  >
-    <div className="revue-form-group">
-      <label htmlFor="member_first_name">First name</label>
-      <input
-        className="revue-form-field block"
-        type="text"
-        name="member[first_name]"
-        id="member_first_name"
-        autoCapitalize="on"
-      />
-    </div>
-    <div className="revue-form-group">
-      <label htmlFor="member_email">Email</label>
-      <input
-        className="revue-form-field block"
-        type="email"
-        name="member[email]"
-        id="member_email"
-      />
-    </div>
-    <div className="revue-form-actions">
-      <input
-        type="submit"
-        defaultValue="Subscribe"
-        name="member[subscribe]"
-        id="member_submit"
-      />
-    </div>
-    <div className="revue-form-footer">
-      <p className=" light-font">
-        <small>
-          By subscribing, you agree with Revue's
-          <a
-            target="_blank"
-            href="https://www.getrevue.co/terms"
-            rel="noopener noreferrer"
-          >
-            Terms of Service{" "}
-          </a>
-          and
-          <a
-            target="_blank"
-            href="https://www.getrevue.co/privacy"
-            rel="noopener noreferrer"
-          >
-            Privacy Policy
-          </a>
-          . Powered by
-          <a
-            href="https://www.getrevue.co/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Revue
-          </a>
-        </small>
-      </p>
-    </div>
-  </form>
-</div>
+                                    <form
+                                        action="https://www.getrevue.co/profile/theryanfurrer/add_subscriber"
+                                        method="post"
+                                        id="revue-form"
+                                        name="revue-form"
+                                        target="_blank"
+                                    >
+                                        <div className="revue-form-group">
+                                        <label htmlFor="member_first_name">First name</label>
+                                        <input
+                                            className="revue-form-field block"
+                                            type="text"
+                                            name="member[first_name]"
+                                            id="member_first_name"
+                                            autoCapitalize="on"
+                                        />
+                                        </div>
+                                        <div className="revue-form-group">
+                                        <label htmlFor="member_email">Email</label>
+                                        <input
+                                            className="revue-form-field block"
+                                            type="email"
+                                            name="member[email]"
+                                            id="member_email"
+                                        />
+                                        </div>
+                                        <div className="revue-form-actions">
+                                        <input
+                                            type="submit"
+                                            defaultValue="Subscribe"
+                                            name="member[subscribe]"
+                                            id="member_submit"
+                                        />
+                                        </div>
+                                        <div className="revue-form-footer">
+                                        <p className=" light-font">
+                                            <small>
+                                            By subscribing, you agree with Revue's
+                                            <a
+                                                target="_blank"
+                                                href="https://www.getrevue.co/terms"
+                                                rel="noopener noreferrer"
+                                            >
+                                                Terms of Service{" "}
+                                            </a>
+                                            and
+                                            <a
+                                                target="_blank"
+                                                href="https://www.getrevue.co/privacy"
+                                                rel="noopener noreferrer"
+                                            >
+                                                Privacy Policy
+                                            </a>
+                                            . Powered by
+                                            <a
+                                                href="https://www.getrevue.co/"
+                                                target="_blank"
+                                                rel="noopener noreferrer"
+                                            >
+                                                Revue
+                                            </a>
+                                            </small>
+                                        </p>
+                                        </div>
+                                    </form>
+                                    </div>
 
-                    </div>
+                                                        </div>
 
 
-                )
-            }
-            {
-                props.text && (
-                    <Markdown
-                        options={{ forceBlock: true, forceWrapper: true }}
-                        className={classNames('sb-markdown', 'sm:text-lg', styles.text ? mapStyles(styles.text) : null, { 'mt-4': props.title })}
-                        data-sb-field-path=".text"
-                    >
-                        {props.text}
-                    </Markdown>
-                )
-            }
-        </div >
-    );
-}
+                                                    )
+                                                }
+                                                {
+                                                    props.text && (
+                                                        <Markdown
+                                                            options={{ forceBlock: true, forceWrapper: true }}
+                                                            className={classNames('sb-markdown', 'sm:text-lg', styles.text ? mapStyles(styles.text) : null, { 'mt-4': props.title })}
+                                                            data-sb-field-path=".text"
+                                                        >
+                                                            {props.text}
+                                                        </Markdown>
+                                                    )
+                                                }
+                                            </div >
+                                        );
+                                    }
 
-function ctaActions(props) {
-    const actions = props.actions || [];
-    if (actions.length === 0) {
-        return null;
-    }
-    const styles = props.styles || {};
-    return (
-        <div className={classNames('w-full', styles.self?.flexDirection === 'row' ? 'lg:w-auto' : null)}>
-            <div className="overflow-x-hidden">
-                <div
-                    className={classNames('flex', 'flex-wrap', 'items-center', '-mx-2', 'lg:flex-nowrap', styles.actions ? mapStyles(styles.actions) : null)}
-                    data-sb-field-path=".actions"
-                >
-                    {actions.map((action, index) => (
-                        <Action key={index} {...action} className="mb-3 mx-2 lg:whitespace-nowrap" data-sb-field-path={`.${index}`} />
-                    ))}
-                </div>
-            </div>
+                                    function ctaActions(props) {
+                                        const actions = props.actions || [];
+                                        if (actions.length === 0) {
+                                            return null;
+                                        }
+                                        const styles = props.styles || {};
+                                        return (
+                                            <div className={classNames('w-full', styles.self?.flexDirection === 'row' ? 'lg:w-auto' : null)}>
+                                                <div className="overflow-x-hidden">
+                                                    <div
+                                                        className={classNames('flex', 'flex-wrap', 'items-center', '-mx-2', 'lg:flex-nowrap', styles.actions ? mapStyles(styles.actions) : null)}
+                                                        data-sb-field-path=".actions"
+                                                    >
+                                                        {actions.map((action, index) => (
+                                                            <Action key={index} {...action} className="mb-3 mx-2 lg:whitespace-nowrap" data-sb-field-path={`.${index}`} />
+                                                        ))}
+                                                    </div>
+                                                </div>
         </div>
     );
 }

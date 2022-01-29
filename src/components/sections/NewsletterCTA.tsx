b8cac6f9b783b9e943fb2e5047ab74a3857ad18d
Stackbit Code Editor <projects@stackbit.com>
2022-01-29 02:46:26 +0000

    NewsletterCTA.tsx: changed

--- src/components/sections/NewsletterCTA.tsx
+++ src/components/sections/NewsletterCTA.tsx
@@ -121,22 +121,22 @@ function ctaBody(props) {
                         </h2>
                         <div id="revue-embed">
                             <form action="https://www.getrevue.co/profile/theryanfurrer/add_subscriber" method="post" id="revue-form" name="revue-form"  target="_blank">
-                                <div class="revue-form-group">
+                                <div className="revue-form-group">
                                     <label for="member_email">Email address</label>
-                                    <input class="revue-form-field" placeholder="Your email address..." type="email" name="member[email]" id="member_email">
+                                    <input className="revue-form-field" placeholder="Your email address..." type="email" name="member[email]" id="member_email">
                                 </div>
-                                <div class="revue-form-group">
-                                    <label for="member_first_name">First name <span class="optional">(Optional)</span></label>
-                                    <input class="revue-form-field" placeholder="First name... (Optional)" type="text" name="member[first_name]" id="member_first_name">
+                                <div className="revue-form-group">
+                                    <label for="member_first_name">First name <span className="optional">(Optional)</span></label>
+                                    <input className="revue-form-field" placeholder="First name... (Optional)" type="text" name="member[first_name]" id="member_first_name">
                                 </div>
-                                <div class="revue-form-group">
-                                    <label for="member_last_name">Last name <span class="optional">(Optional)</span></label>
-                                    <input class="revue-form-field" placeholder="Last name... (Optional)" type="text" name="member[last_name]" id="member_last_name">
+                                <div className="revue-form-group">
+                                    <label for="member_last_name">Last name <span className="optional">(Optional)</span></label>
+                                    <input className="revue-form-field" placeholder="Last name... (Optional)" type="text" name="member[last_name]" id="member_last_name">
                                 </div>
-                                <div class="revue-form-actions">
+                                <div className="revue-form-actions">
                                     <input type="submit" value="Subscribe" name="member[subscribe]" id="member_submit">
                                 </div>
-                                <div class="revue-form-footer">By subscribing, you agree with Revue’s <a target="_blank" href="https://www.getrevue.co/terms">Terms of Service</a> and <a target="_blank" href="https://www.getrevue.co/privacy">Privacy Policy</a>.</div>
+                                <div className="revue-form-footer">By subscribing, you agree with Revue’s <a target="_blank" href="https://www.getrevue.co/terms">Terms of Service</a> and <a target="_blank" href="https://www.getrevue.co/privacy">Privacy Policy</a>.</div>
                             </form>
                         </div>
                     </div>

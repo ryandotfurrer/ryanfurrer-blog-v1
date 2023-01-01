21ac5fc03cb7620a9b7ac063225b15d3a76080aa
Stackbit Code Editor <projects@stackbit.com>
2023-01-01 14:23:19 +0000

    NewsletterCTA.tsx: changed

--- src/components/sections/NewsletterCTA.tsx
+++ src/components/sections/NewsletterCTA.tsx
@@ -135,7 +135,7 @@ function ctaBody(props) {
                         >
                             {props.text}
                         </Markdown>
-                        <p>Sign up on <a href="https://ryanfurrer.substack.com" rel="noopener noreferrer" style="">Substack</a> to be notified whenever I sent out a new email - typically 2 to 4 a month.</p>
+                        <p>Sign up on <a href="https://ryanfurrer.substack.com" rel="noopener noreferrer" style="underline">Substack</a> to be notified whenever I sent out a new email - typically 2 to 4 a month.</p>
 
                     </div>
                 )

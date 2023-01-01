20eb0ecbb47b307454a6b7ba3f8f0509c3c2e3f0
Stackbit Code Editor <projects@stackbit.com>
2023-01-01 14:21:16 +0000

    NewsletterCTA.tsx: changed

--- src/components/sections/NewsletterCTA.tsx
+++ src/components/sections/NewsletterCTA.tsx
@@ -136,7 +136,6 @@ function ctaBody(props) {
                             {props.text}
                         </Markdown>
                         <p>Sign up on Substack to be notified whenever I sent out a new email - typically 2 to 4 a month.</p>
-                        <button>Sign up now</button>
 
                         
                     </div>
